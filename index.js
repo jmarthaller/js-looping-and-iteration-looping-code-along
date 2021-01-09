82964f57331ec655abe2c333b685b0487e0a90fc
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-09 15:03:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, beemo) {
+   let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(`Thank you, ${array[i]}, for the wonderful ${beemo} gift!`);
+    }
+    return result;
+}
+
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
